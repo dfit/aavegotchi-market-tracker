75020f29256210cf0ed38f2d380ff7bed902a25c
David F <david.fitoussi@decathlon.com>
2022-07-06 10:36:00 +0200

    fix(tracker): tracker initialization

--- src/userTrackersManager.js
+++ src/userTrackersManager.js
@@ -11,6 +11,12 @@ module.exports = {
   },
   deleteTracker(userId, trackerType) {
     const trackerTypeMessage = `/${trackerType}`
+    try {
+      dbManager.db.getData(trackerTypeMessage);
+    } catch(error) {
+      console.log(`${trackerTypeMessage} doesn't exists yet`)
+      return
+    }
     dbManager.db.delete(`/${trackerType}[${dbManager.db.getIndex(trackerTypeMessage, userId)}]`)
   },
   getUserTracker(userId, trackerType) {
